3a409167cb3b27aac4ad2df2492f4432110b403b
JoaoGuilherme2909 <joaoguilhermesantos2909@gmail.com>
2022-03-02 12:53:20 -0400

    Added a server

---
+++ server.js
@@ -0,0 +1,16 @@
+//variables, constats, instances
+const express = require('express');
+const app = express();
+
+//configs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
+
+//functions
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(8080, () => {
+    console.log("Rodando")
+})
