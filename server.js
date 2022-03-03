a3667a8710b3d25ff1aea96822663138df86a901
JoaoGuilherme2909 <joaoguilhermesantos2909@gmail.com>
2022-03-02 21:52:07 -0400

    Added a server

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.static(__dirname + '/public'))
 
 //functions
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 })
 
 app.listen(8080, () => {
-    console.log("Rodando")
+  console.log("Rodando")
 })
